9e8178a8232c600cbaee1035c87d73b303188ee9
VlanAni <117458114+VlanAni@users.noreply.github.com>
2025-02-22 17:37:21 +0700

    Add files via upload
    Задача на 5. Все тесты пройдены. Добавлено определение.

--- src/Tier2/Writer.hs
+++ src/Tier2/Writer.hs
@@ -4,4 +4,12 @@ import Control.Monad.Writer
 import Tier0.Writer (Tree (..))
 
 collectAndSumInOrder :: Num a => Tree a -> Writer (Sum a) [a]
-collectAndSumInOrder = undefined
+collectAndSumInOrder (Leaf a) = do
+    tell (Sum a) 
+    return (a:[])
+collectAndSumInOrder (Branch left a right) = do 
+    left_vert <- collectAndSumInOrder left 
+    tell (Sum a) 
+    right_vert <- collectAndSumInOrder right 
+    return (left_vert ++ (a:[]) ++ right_vert)  
+
