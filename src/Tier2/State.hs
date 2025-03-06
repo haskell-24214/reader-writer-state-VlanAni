b3cd7618a48ffe1dba1b301c2b7659ca8da89705
VlanAni <117458114+VlanAni@users.noreply.github.com>
2025-03-06 12:17:28 +0700

    Add files via upload
    Выполнения задания про калькулятор на монаде State

--- src/Tier2/State.hs
+++ src/Tier2/State.hs
@@ -9,34 +9,97 @@ emptyRegisters = Registers 0 0 False 0
 type Calculation = State Registers Int
 
 plus :: Calculation
-plus = undefined
+plus = do
+  currRegs <- get 
+  let ax_now = ax currRegs
+      bx_now = bx currRegs 
+      acc_now = bx_now + ax_now 
+  put currRegs { acc = acc_now, blink = False } 
+  return acc_now
 
 minus :: Calculation
-minus = undefined
+minus = do
+  currRegs <- get 
+  let ax_now = ax currRegs
+      bx_now = bx currRegs 
+      acc_now = ax_now - bx_now 
+  put currRegs { acc = acc_now, blink = False } 
+  return acc_now
+
 
 productS :: Calculation
-productS = undefined
+productS = do
+  currRegs <- get 
+  let ax_now = ax currRegs
+      bx_now = bx currRegs 
+      acc_now = bx_now * ax_now 
+  put currRegs { acc = acc_now, blink = False } 
+  return acc_now
 
-div :: Calculation
-div = undefined
+
+divit :: Calculation
+divit = do
+  currRegs <- get 
+  let ax_now = ax currRegs
+      bx_now = bx currRegs 
+  if (bx_now == 0) 
+    then do  
+      put currRegs { ax = 0, bx = 0, acc = 0, blink = False } 
+      return 0 
+    else do 
+      let acc_now = ax_now `div` bx_now 
+      put currRegs { acc = acc_now, blink = False }
+      return acc_now
+
 
 swap :: Calculation
-swap = undefined
+swap = do
+  currRegs <- get 
+  let ax_now = ax currRegs 
+      bx_now = bx currRegs 
+      acc_now = acc currRegs
+  put currRegs { ax = bx_now, bx = ax_now }
+  return acc_now
 
 blinkS :: Calculation
-blinkS = undefined
+blinkS = do 
+  currRegs <- get 
+  let blink_now = blink currRegs 
+      blink_new = not blink_now  
+      acc_now = acc currRegs
+  put currRegs { blink = blink_new }
+  return acc_now
 
 accS :: Calculation
-accS = undefined
-
+accS = do 
+  currRegs <- get
+  let acc_now = acc currRegs 
+      blink_now = blink currRegs
+  if blink_now
+    then do 
+      put currRegs { bx = acc_now, blink = False }
+    else do 
+      put currRegs { ax = acc_now, blink = True }
+  return acc_now
+  
 number :: Int -> Calculation
-number x = undefined
+number x = do
+  currRegs <- get
+  let blink_now = blink currRegs 
+      acc_now = acc currRegs
+  if blink_now
+    then do 
+      put currRegs { bx = x, blink = False }
+    else do 
+      put currRegs { ax = x, blink = True }  
+  return acc_now
 
 commandToCalculation :: String -> Calculation
 commandToCalculation s =
   case s of
     "+" -> plus
     "-" -> minus
+    "/" -> divit 
     "*" -> productS
     "swap" -> swap
     "blink" -> blinkS
